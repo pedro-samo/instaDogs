8e20817dc1e05b873dba279903667ae5b36b14eb
Pedro Morais <pedro.s.morais@gmail.com>
2021-06-12 18:29:41 -0300

    feat: feed modal

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import FeedModal from "./FeedModal";
+import FeedPhotos from "./FeedPhotos";
 
 const Feed = () => {
+  const [modalPhoto, setModalPhoto] = React.useState(null);
+
   return (
     <div>
-      Feed
+      {modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto}/>}
+      <FeedPhotos setModalPhoto={setModalPhoto}/>
     </div>
-  )
-}
+  );
+};
 
 export default Feed;
--- src/api.js
+++ src/api.js
@@ -1,64 +1,84 @@
-export const API_URL = 'https://dogsapi.origamid.dev/json';
+export const API_URL = "https://dogsapi.origamid.dev/json";
 
 export function TOKEN_POST(body) {
   return {
-    url: API_URL + '/jwt-auth/v1/token',
+    url: API_URL + "/jwt-auth/v1/token",
     options: {
-      method: 'POST',
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(body),
-    }
-  }
+    },
+  };
 }
 
 export function TOKEN_VALIDATE_POST(token) {
   return {
-    url: API_URL + '/jwt-auth/v1/token/validate',
+    url: API_URL + "/jwt-auth/v1/token/validate",
     options: {
-      method: 'POST',
+      method: "POST",
       headers: {
-        Authorization: 'Bearer ' + token,
+        Authorization: "Bearer " + token,
       },
-    }
-  }
+    },
+  };
 }
 
-export function USER_GET (token) {
+export function USER_GET(token) {
   return {
-    url: API_URL + '/api/user',
+    url: API_URL + "/api/user",
     options: {
-      method: 'GET',
+      method: "GET",
       headers: {
-        Authorization: 'Bearer ' + token,
+        Authorization: "Bearer " + token,
       },
-    }
-  }
+    },
+  };
 }
 
-export function USER_POST (body) {
+export function USER_POST(body) {
   return {
-    url: API_URL + '/api/user',
+    url: API_URL + "/api/user",
     options: {
-      method: 'POST',
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(body),
-    }
-  }
+    },
+  };
 }
 
-export function PHOTO_POST (formData, token) {
+export function PHOTO_POST(formData, token) {
   return {
-    url: API_URL + '/api/photo',
+    url: API_URL + "/api/photo",
     options: {
-      method: 'POST',
+      method: "POST",
       headers: {
-        Authorization: 'Bearer ' + token,
+        Authorization: "Bearer " + token,
       },
-      body: formData
-    }
-  }
+      body: formData,
+    },
+  };
+}
+
+export function PHOTOS_GET({ page, total, user }) {
+  return {
+    url: `${API_URL}/api/photo/?_page=${page}&_total=${total}&_user=${user}`,
+    options: {
+      method: "GET",
+      cache: "no-store",
+    },
+  };
+}
+
+export function PHOTO_GET(id) {
+  return {
+    url: `${API_URL}/api/photo/${id}`,
+    options: {
+      method: "GET",
+      cache: "no-store",
+    },
+  };
 }
